571f1074eb58265b1ab4b34c74e5c99f96bdbd96
DingSoung <>
2019-11-26 00:20:14 +0800

    chore: text c for swift

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "YogaLayout",
+    platforms: [
+        .iOS(.v8)
+    ],
+    products: [
+        .library(name: "YogaLayout", targets: ["YogaLayout"])
+    ],
+    targets: [
+        .target(name: "YogaLayout", 
+                path: "Sources",
+                publicHeadersPath: "../Sources",
+                cSettings: [
+                    .headerSearchPath("Sources")
+                ])
+    ],
+    swiftLanguageVersions: [
+        .version("5")
+    ],
+    cxxLanguageStandard: .gnucxx11
+)
