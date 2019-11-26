ed3929a7e961d15b73fdd653d46f54ae495280ff
DingSoung <>
2019-11-26 23:41:06 +0800

    chore:

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .target(name: "YogaLayout", 
                 path: "Sources",
-                publicHeadersPath: "../Sources"
+                publicHeadersPath: "../Sources/publicHeader"
                 //cSettings: [ .headerSearchPath("Sources") ]
             )
     ],
